9869f0d8eb8040d8878c63cecfab4a90e14fb6d3
bamcortezz <2022312593@dhvsu.edu.ph>
2024-11-14 23:19:24 +0800

    changing the style

--- index.php
+++ index.php
@@ -1,70 +1,68 @@
-<?php 
+<?php
 include_once 'config/settings-config.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="src/css/bootstrap.css">
-    <link rel="stylesheet" href="src/js/bootstrap.js">
-    <link rel="stylesheet" href="src/css/style.css">
-</head>
-<body>
-<div class="wrapper">
-    <div class="container main">
-      <div class="row">
-        <div class="col-md-5 side-image">
-          <img src="src/image/1.jpg" alt="">
-        </div>
-        <div class="col-md-7 right">
-          
-          <form id="login-form" action="dashboard/authentication/class.php" method="POST">
-            <div class="input-box">
-              <h1>LOGIN</h1>
-              <div class="input-field">
-                <input type="email" name="email" required class="form-control" placeholder="Enter Email">
-              </div>
-              <div class="input-field">
-                <input type="password" name="password" required class="form-control" placeholder="Enter Password">
-              </div>
-              <div class="input-field">
-                <button type="submit" name="btn-signin" class="submit">Sign in</button>
-              </div>
-              <div class="signin">
-                <span>Need to register? <a href="#" id="show-registration">Register here</a></span>
-              </div>
-            </div>
-          </form>
 
-          <form id="registration-form" action="dashboard/authentication/class.php" method="POST" style="display: none;">
-            <div class="input-box">
-              <h1>REGISTRATION</h1>
-              <div class="input-field">
-                <input type="text" name="username" placeholder="Enter Username" required class="form-control">
-              </div>
-              <div class="input-field">
-                <input type="email" name="email" required class="form-control" placeholder="Enter Email">
-              </div>
-              <div class="input-field">
-                <input type="password" name="password" required class="form-control" placeholder="Enter Password">
-              </div>
-              <div class="input-field">
-                <button type="submit" name="btn-signup" class="submit">Sign Up</button>
-              </div>
-              <div class="signin">
-                <span>Already have an account? <a href="#" id="show-login">Login here</a></span>
-              </div>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="src/css/bootstrap.css">
+  <link rel="stylesheet" href="src/js/bootstrap.js">
+</head>
+
+<body>
+  <div class="wrapper">
+    <div class="container main">
+      <div class="col-md-5 py-5 right card">
+
+        <form id="login-form" action="dashboard/authentication/class.php" method="POST">
+          <div class="input-box">
+            <h1>LOGIN</h1>
+            <div class="input-field">
+              <input type="email" name="email" required class="form-control" placeholder="Enter Email">
             </div>
-          </form>
-        </div>
+            <div class="input-field">
+              <input type="password" name="password" required class="form-control" placeholder="Enter Password">
+            </div>
+            <div class="input-field">
+              <button type="submit" name="btn-signin" class="submit">Sign in</button>
+            </div>
+            <div class="signin">
+              <span>Need to register? <a href="#" id="show-registration">Register here</a></span>
+            </div>
+          </div>
+        </form>
+
+        <form id="registration-form" action="dashboard/authentication/class.php" method="POST" style="display: none;">
+          <div class="input-box">
+            <h1>REGISTRATION</h1>
+            <div class="input-field">
+              <input type="text" name="username" placeholder="Enter Username" required class="form-control">
+            </div>
+            <div class="input-field">
+              <input type="email" name="email" required class="form-control" placeholder="Enter Email">
+            </div>
+            <div class="input-field">
+              <input type="password" name="password" required class="form-control" placeholder="Enter Password">
+            </div>
+            <div class="input-field">
+              <button type="submit" name="btn-signup" class="submit">Sign Up</button>
+            </div>
+            <div class="signin">
+              <span>Already have an account? <a href="#" id="show-login">Login here</a></span>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   </div>
 
-  
+
+
   <script src="src/js/script.js"></script>
 </body>
+
 </html>
